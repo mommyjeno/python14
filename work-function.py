ed9512a48799fd51d49d2dc4e52cc4729b0c7740
mommyjeno <50241@cru.ac.th>
2022-07-26 14:50:02 +0700

    function

---
+++ work-function.py
@@ -0,0 +1,16 @@
+#คำสั่งหาพื้นที่สี่เหลี่ยม
+def square(width,height):
+    ans = width*height
+    print(ans)
+square(3,5)
+#คำสั่งหาพื้นที่สามเหลี่ยม
+def triangle(base,height):
+    ans = (base*height)/2
+    print(ans)
+triangle(4,6)
+#คำสั่งหาพื้นที่วงกลม
+def circle(radius):
+    ans = (3.14*radius*radius)
+    print(ans)
+circle(9)
+#ชื่อน.ส.ชนิกานต์ กิจประเสริฐ เลขที่ 36 ชั้น ม.6/14
